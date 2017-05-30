ba7812af04cef29b0804095a205eb890f836b3c7
iKatha <katha.ftw@gmail.com>
2017-05-30 12:17:05 +0200

    Zamiana wyrażenia oraz obliczanie wyniku, obsługa błędu dzielenia przez zero lub braku podania = na koncu wyrazenia

---
+++ KalkulatorONP/src/Main.java
@@ -0,0 +1,29 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		SterownikONP onp = new SterownikONP();
+		String wyrazenie="";
+		double wynik=0;
+		for(int i=0;i<args.length;i++) {
+			System.out.println("///////////////////////W Y R A ¯ E N I E  "+(i+1)+"////////////////////////");
+			System.out.println("wyra¿enie: " + args[i]);
+			try {
+				wyrazenie=onp.zamienNaONP(args[i]);
+				System.out.println("wyra¿enie onp: " + wyrazenie);
+				wynik=onp.obliczONP(wyrazenie);
+				System.out.println("wynik: "+ wynik);
+				System.out.println("///////////////////////////////////////////////////////////////////");
+				System.out.println();
+			} catch (Exception e) {
+				System.out.println("ERROR: "+e.getMessage());
+				System.out.println("///////////////////////////////////////////////////////////////////");
+				System.out.println();
+			}
+			
+		}
+		
+	}
+
+}
---
+++ KalkulatorONP/src/Stos.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class Stos<T> {
+	
+	private LinkedList<T> stos = new LinkedList<T>();
+	public void push(T e) { stos.addFirst(e); }
+	public T top() { return stos.getFirst(); }
+	public T pop() { return stos.removeFirst(); }
+	public boolean isEmpty() { return stos.isEmpty(); }
+	public String toString() { return stos.toString(); }
+	
+}
